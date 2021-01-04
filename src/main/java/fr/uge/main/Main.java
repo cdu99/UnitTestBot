1e87bf71cc79d53e0d28a52848bf0aa367a73aba
Calvin Du <calvin.du99@gmail.com>
2021-01-04 12:34:57 +0100

    Add JDA dependency

---
+++ src/main/java/fr/uge/main/Main.java
@@ -0,0 +1,15 @@
+package fr.uge.main;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA builder = JDABuilder.createDefault("Nzk1NjA4NDAyODAwOTM0OTgz.X_L2EA.XWr5S82moiVmdrF-PvZDBhMX-NI")
+                .setActivity(Activity.playing("WIP"))
+                .build();
+    }
+}

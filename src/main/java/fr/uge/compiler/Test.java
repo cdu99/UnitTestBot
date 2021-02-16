1f8feacc6b0612ce03667791140f10582c018ecf
Calvin Du <calvin.du99@gmail.com>
2021-02-17 00:34:41 +0100

    Compiler test

---
+++ src/main/java/fr/uge/compiler/Test.java
@@ -0,0 +1,10 @@
+package fr.uge.compiler;
+
+import javax.tools.*;
+
+class Test {
+    public static void main(String[] args) {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        System.out.println(compiler);
+    }
+}

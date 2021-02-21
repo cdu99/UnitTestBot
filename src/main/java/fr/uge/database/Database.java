5168779a34c7eb130f4e051ef8e4d248af3e66c2
Calvin Du <calvin.du99@gmail.com>
2021-02-21 16:28:23 +0100

    Fix DATABASE creating empty lines

--- src/main/java/fr/uge/database/Database.java
+++ src/main/java/fr/uge/database/Database.java
@@ -2,6 +2,8 @@ package fr.uge.database;
 
 import org.jdbi.v3.core.Jdbi;
 
+import java.util.Objects;
+
 public class Database {
     public static final String IN_MEMORY_DB = "jdbc:sqlite::memory:";
     public static final String UNIT_TEST_DATABASE = "jdbc:sqlite:unit-test.db";
@@ -29,6 +31,7 @@ public class Database {
     }
 
     public void insertTestResultBean(TestResult testResult) {
+        Objects.requireNonNull(testResult);
         jdbi.useHandle(handle -> handle.createUpdate("insert into test_result " +
                 "(student, question, test, result) values " +
                 "(:student, :question, :test, :result)")
